7f396bd3e1d405876ba93b59b23cf76221b011c7
Corentin Chary <c.chary@criteo.com>
2017-01-24 10:12:38 +0100

    [criteo] bump to 1.1.0-criteo4

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ install_files += init_scripts
 try:
     setup(
       name='carbon',
-      version='1.1.0',
+      version='1.1.0-criteo4',
       url='http://graphiteapp.org/',
       author='Chris Davis',
       author_email='chrismd@gmail.com',
